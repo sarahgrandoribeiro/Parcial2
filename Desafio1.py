7b8f3c7f6c538f30a0e4fdce61ea79d2aaf98f35
sarahgrandoribeiro <sarah.grando.ribeiro@escola.pr.gov.br>
2026-03-31 07:44:13 -0300

    Desafio1.py

---
+++ Desafio1.py
@@ -0,0 +1,6 @@
+# Selecionar o primeiro número decimal 
+n1 = float(input('Primeiro número:'))
+# Selecionar o segundo número decimal 
+n2 = float(input('Segundo número:'))
+# Realizar a soma e mostrar o resultado 
+print('A soma é', n1 + n2)
