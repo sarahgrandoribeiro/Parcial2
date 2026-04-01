74df1f2f6d86318b3a22157955e9ee49a3bd9c6c
sarahgrandoribeiro <sarah.grando.ribeiro@escola.pr.gov.br>
2026-04-01 14:22:20 -0300

    Update Desafio3.py

---
+++ Desafio3.py
@@ -0,0 +1,7 @@
+nomes = [] # Cria uma lista vazia para armazenar os nomes
+nomes.append(input("Digite o 1º nome: ")) # Adiciona o primeiro nome à lista
+nomes.append(input("Digite o 2º nome: ")) # Adiciona o segundo nome à lista
+nomes.append(input("Digite o 3º nome: ")) # Adiciona o terceiro nome à lista
+nomes.append(input("Digite o 4º nome: ")) # Adiciona o quarto nome à lista
+nomes.append(input("Digite o 5º nome: ")) # Adiciona o quinto nome à lista
+print("Os nomes digitados foram:", nomes) # Imprime a lista completa de nomes na tela
